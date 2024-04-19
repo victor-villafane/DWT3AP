789cdbf53f5db5960ffba41dec7fd327b1aa27dd
victor-villafane <victor.villafane@davinci.edu.ar>
2024-04-19 15:01:26 -0300

    Aplicamos metodos get y agregamos metodos a nuestro objeto comic

--- class/Comic.php
+++ class/Comic.php
@@ -2,18 +2,18 @@
 
 class Comic{
     //atributos
-    public $id;                      //solo pueden acceder los metodos de la propia clase y los metodos de los hijos de esa clase
-    public $personaje;
-    public $serie;
-    public $volumen;
-    public $numero;
-    public $titulo;
-    public $publicacion;
-    public $guion;
-    public $arte;
-    public $bajada;
-    public $portada;
-    public $precio;
+    protected $id;                      //solo pueden acceder los metodos de la propia clase y los metodos de los hijos de esa clase
+    protected $personaje;
+    protected $serie;
+    protected $volumen;
+    protected $numero;
+    protected $titulo;
+    protected $publicacion;
+    protected $guion;
+    protected $arte;
+    protected $bajada;
+    protected $portada;
+    protected $precio;
     //metodos
     public function catalogo_completo(){
         $catalogo = [];
@@ -23,7 +23,7 @@ class Comic{
         foreach ($productosArray as $value) {
 
             //creo una instancia de comic -> ahora tengo un objeto comic
-            $comic = new Comic();
+            $comic = new self();   //self
             //relleno los atributos
             $comic->id = $value->id;
             $comic->personaje = $value->personaje;
@@ -64,5 +64,137 @@ class Comic{
         }
     
         return $personajes;
+    }
+
+    public function modificacionSerie(){
+        //cambio el - por un " "
+        $tituloConEspacio = str_replace("-", " ", $this->serie);
+        //explode divide por un caracter indicado
+        $arrayTitulo = explode(" ", $tituloConEspacio);
+        //paso ambas palabras a mayusculas
+        for( $i = 0; $i < count($arrayTitulo) ; $i++ ){
+            $arrayTitulo[$i] = ucfirst($arrayTitulo[$i]);
+        }
+        //implode lo une utilizando el caracter que le indicamos
+        $tituloCorregido = implode(" ", $arrayTitulo);
+        return $tituloCorregido;
+    }
+
+    public function getBajadaResumida( $cantidad = 20 ){
+        //divido el texto usando explode 
+        //str_word_count()
+        $arrayTexto = explode(" ", $this->bajada); //
+        $textoResumido = [];
+
+        foreach ($arrayTexto as $key => $value) {
+            if( $key < $cantidad ){
+                $textoResumido []= $value;
+            }else{
+                break;
+            }
+        }
+        return implode(" ", $textoResumido)."...";
+    }
+
+
+    //get -> sirven para obtener el valor del atributo
+    public function getId(){
+        return $this->id;
+    }
+    public function getPersonaje(){
+        return $this->personaje;
+    }
+    //set -> sirver para cambiar el valor del atributo
+
+    /**
+     * Get the value of volumen
+     */ 
+    public function getVolumen()
+    {
+        return $this->volumen;
+    }
+
+    /**
+     * Get the value of serie
+     */ 
+    public function getSerie()
+    {
+        return $this->serie;
+    }
+
+    /**
+     * Get the value of numero
+     */ 
+    public function getNumero()
+    {
+        return $this->numero;
+    }
+
+    /**
+     * Get the value of titulo
+     */ 
+    public function getTitulo()
+    {
+        return $this->titulo;
+    }
+
+    /**
+     * Get the value of publicacion
+     */ 
+    public function getPublicacion()
+    {
+        return $this->publicacion;
+    }
+
+    /**
+     * Get the value of guion
+     */ 
+    public function getGuion()
+    {
+        return $this->guion;
+    }
+
+    /**
+     * Get the value of arte
+     */ 
+    public function getArte()
+    {
+        return $this->arte;
+    }
+
+    /**
+     * Get the value of bajada
+     */ 
+    public function getBajada()
+    {
+        return $this->bajada;
+    }
+
+    /**
+     * Get the value of portada
+     */ 
+    public function getPortada()
+    {
+        return $this->portada;
+    }
+
+    /**
+     * Get the value of precio
+     */ 
+    public function getPrecio()
+    {
+        return $this->precio;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
     }
 }
--- views/comics.php
+++ views/comics.php
@@ -1,40 +1,40 @@
 <?php
-    include_once "libraries/funciones.php";
-    $serieSeleccionada = $_GET["serie"];
-    //$comics = $productos[$serieSeleccionada]; 
-    $comics = (new Comic())->catalogo_x_personaje($serieSeleccionada);
-    //index.php?sec=comics&serie=batman
-    // echo "<pre>";
-    // print_r($comics[1]["portada"]);
-    // echo "</pre>"
+include_once 'libraries/funciones.php';
+$serieSeleccionada = $_GET['serie'];
+//$comics = $productos[$serieSeleccionada];
+$comics = (new Comic())->catalogo_x_personaje($serieSeleccionada);
+//index.php?sec=comics&serie=batman
+// echo "<pre>";
+// print_r($comics[1]["portada"]);
+// echo "</pre>"
 ?>
 
 
-<h1 class="text-center my-5"><?= modificacionTitulo($serieSeleccionada) ?></h1>
-    <div class="row">
-<?php foreach ($comics as $comic) { ?>
+<h1 class="text-center my-5"><?= $comics[0]->modificacionSerie() ?></h1>
+<div class="row">
+    <?php foreach ($comics as $comic) { ?>
 
 
-<div class="col-3">
-    <div class="card mb-3">
-        <img class="card-img-top" src="img/covers/<?= $comic->portada ?>">
-        <div class="card-body">
-            <p class="fs-6 m-0 fw-bold text-danger"><?= $comic->serie ?></p>
-            <h5 class="card-title"><?= $comic->titulo ?></h5>
-            <p class="card-text"><?= recortarDescripcion($comic->bajada) ?></p>
-        </div>
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">Guion: <?= $comic->guion ?></li>
-            <li class="list-group-item">Arte: <?= $comic->arte ?></li>
-            <li class="list-group-item">Publicacion: <?= $comic->publicacion ?></li>
-        </ul>
-        <div class="card-body">
-            <div class="fs-3 mb-3 fw-bold text-center text-danger">$<?= $comic->precio ?></div>
-            <a href="index.php?sec=comic&id=<?=$comic->id?>" class="btn btn-danger w-100 fw-bold">COMPRAR</a>
+    <div class="col-3">
+        <div class="card mb-3">
+            <img class="card-img-top" src="img/covers/<?= $comic->getPortada() ?>">
+            <div class="card-body">
+                <p class="fs-6 m-0 fw-bold text-danger"><?= $comic->getSerie() ?></p>
+                <h5 class="card-title"><?= $comic->getTitulo() ?></h5>
+                <p class="card-text"><?= $comic->getBajadaResumida() ?></p>
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Guion: <?= $comic->getGuion() ?></li>
+                <li class="list-group-item">Arte: <?= $comic->getArte() ?></li>
+                <li class="list-group-item">Publicacion: <?= $comic->getPublicacion() ?></li>
+            </ul>
+            <div class="card-body">
+                <div class="fs-3 mb-3 fw-bold text-center text-danger">$<?= $comic->getPrecio() ?></div>
+                <a href="index.php?sec=comic&id=<?= $comic->getId() ?>" class="btn btn-danger w-100 fw-bold">COMPRAR</a>
+            </div>
         </div>
     </div>
-</div>
 
 
-<?php } ?>
+    <?php } ?>
 </div>
